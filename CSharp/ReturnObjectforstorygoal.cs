5d907f18ded92c6d7a28eb1d74902509b1bedf51
VioletTheCheeseGuy <103865493+alphavr1@users.noreply.github.com>
2025-12-25 00:29:34 -0500

    Create ReturnObjectforstorygoal.cs
    No longer used currently

---
+++ CSharp/ReturnObjectforstorygoal.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Nautilus;
+using AV;
+
+namespace Violet.AV.StoryGoalStuff
+{
+
+
+    internal class ReturnObjectforstorygoal : MonoBehaviour
+    {
+
+
+
+
+
+    }
+}
